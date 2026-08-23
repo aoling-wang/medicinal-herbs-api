d352ab9553ade02ced957016821305fdd120a641
Kevin Wang <aoling.wang@gmail.com>
2026-08-23 19:58:19 -0400

    Adding DB to app module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HerbsModule } from './herbs/herbs.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [HerbsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    
+    HerbsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
