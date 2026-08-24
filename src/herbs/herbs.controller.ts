221ec68ff383b94fb448381065af0bdb8d01c1e9
Kevin Wang <aoling.wang@gmail.com>
2026-08-23 21:31:35 -0400

    Adding controller and service to herbs module

--- src/herbs/herbs.controller.ts
+++ src/herbs/herbs.controller.ts
@@ -1,4 +1,60 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    Post,
+    Delete,
+    Param,
+} from '@nestjs/common';
+
+import { HerbsService } from './herbs.service';
 
 @Controller('herbs')
-export class HerbsController {}
+export class HerbsController {
+    constructor(
+        private readonly herbsService: HerbsService,
+    ) {}
+
+    @Get()
+    findAll() {
+        return this.herbsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.herbsService.findOne(id);
+    }
+
+    @Post()
+    create(
+        @Body() body: {
+            name: string; 
+            activeCompounds: string[]; 
+            medicinalUses: string[]; 
+            targetSystem: string[]; 
+            useableParts: string[]; 
+            preparationMethods: string[]; 
+            interactions: { compound: string; effect: string }[]; 
+            contraindications: string[] 
+        },
+    ) {
+        return this.herbsService.create(
+            {
+                name: body.name,
+                activeCompounds: body.activeCompounds,
+                medicinalUses: body.medicinalUses,
+                targetSystem: body.targetSystem,
+                useableParts: body.useableParts,
+                preparationMethods: body.preparationMethods,
+                interactions: body.interactions,
+                contraindications: body.contraindications
+            }
+        );
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.herbsService.remove(id);
+    }
+}
+
--- src/herbs/herbs.service.ts
+++ src/herbs/herbs.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Herb } from './herbs.entity';
 
 @Injectable()
-export class HerbsService {}
+export class HerbsService {
+    constructor(
+        @InjectRepository(Herb)
+        private readonly herbsRepository: Repository<Herb>,
+    ) {}
+
+    findAll() {
+        return this.herbsRepository.find();
+    }
+
+    findOne(id: string) {
+        return this.herbsRepository.findOneBy({ id: Number(id) });
+    }
+
+    create({
+        name, 
+        activeCompounds, 
+        medicinalUses, 
+        targetSystem,
+        useableParts,
+        preparationMethods,
+        interactions,
+        contraindications,
+    }: {
+        name: string, 
+        activeCompounds: string[],
+        medicinalUses: string[], 
+        targetSystem: string[], 
+        useableParts: string[], 
+        preparationMethods: string[], 
+        interactions: { compound: string; effect: string }[], 
+        contraindications: string[]
+    }) {
+        const herb = this.herbsRepository.create({
+            name,
+            activeCompounds,
+            medicinalUses,
+            targetSystem,
+            useableParts,
+            preparationMethods,
+            interactions,
+            contraindications,
+        });
+
+        return this.herbsRepository.save(herb);
+    }
+
+    remove(id: string) {
+        return this.herbsRepository.delete(id);
+    }
+}
