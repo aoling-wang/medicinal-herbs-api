cb2d08953750872e6e724e0c6f369405feacf792
Kevin Wang <aoling.wang@gmail.com>
2026-09-20 22:36:08 -0400

    Debug unit tests

--- src/herbs/herbs.controller.spec.ts
+++ src/herbs/herbs.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HerbsController } from './herbs.controller';
-import {describe, it, expect, beforeEach} from '@jest/globals';
+import { HerbsService } from './herbs.service';
+import { describe, it, expect, beforeEach, jest } from '@jest/globals';
 
 describe('HerbsController', () => {
   let controller: HerbsController;
@@ -8,6 +9,17 @@ describe('HerbsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HerbsController],
+      providers: [
+        {
+          provide: HerbsService,
+          useValue: {
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<HerbsController>(HerbsController);
--- src/herbs/herbs.service.spec.ts
+++ src/herbs/herbs.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { HerbsService } from './herbs.service';
+import { Herb } from './herbs.entity';
+import { describe, it, expect, beforeEach, jest } from '@jest/globals';
 
 describe('HerbsService', () => {
   let service: HerbsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HerbsService],
+      providers: [
+        HerbsService,
+        {
+          provide: getRepositoryToken(Herb),
+          useValue: {
+            find: jest.fn(),
+            findOneBy: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<HerbsService>(HerbsService);
